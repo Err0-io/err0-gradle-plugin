8c7cc81d5d2d1088df4672228151a7890c1fb5b2
Ben Gould <ben@amb1ent.org>
2022-12-14 12:13:21 +0000

    fixed.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	`java-gradle-plugin`
 	`maven-publish`
+	id("com.gradle.plugin-publish") version "1.1.0"
 }
 
 group = "io.err0"
@@ -20,10 +21,19 @@ dependencies {
 	implementation("com.google.code.gson:gson:2.8.7")
 }
 
+pluginBundle {
+	website = "https://github.com/Err0-io/err0-gradle-plugin"
+	vcsUrl = "https://github.com/Err0-io/err0-gradle-plugin.git"
+	tags = listOf("observability")
+}
+
 gradlePlugin {
+
 	plugins {
 		create("err0Plugin") {
 			id = "io.err0.err0-gradle-plugin"
+			displayName = "Gradle plugin for err0"
+			description = "A plugin to enable err0agent to be run with gradle commands"
 			implementationClass = "io.err0.gradle.Err0Plugin"
 		}
 	}
